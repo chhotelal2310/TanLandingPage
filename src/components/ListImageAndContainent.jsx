a6345b1daa5d70b55e25d15caad15eaf2c6a30c1
chhotelal2310 <chhotupatel705@gmail.com>
2025-02-11 23:02:44 +0530

    update

--- src/components/ListImageAndContainent.jsx
+++ src/components/ListImageAndContainent.jsx
@@ -3,33 +3,32 @@ import sun1Iamge from "../assets/TanImage/Sun1.jpg";
 import hrImage from "../assets/TanImage/hrImage.avif";
 import BlockImage1 from "../assets/TanImage/BlockImage1.jpg";
 import BlockImage2 from "../assets/TanImage/BlockImage2.jpg";
-// import BlockImage3 from "../assets/TanImage/BlockImage3.jpeg";
 
 const ListImageAndContainent = () => {
   return (
     <>
-      <div className="w-full place-items-center bg-green-500">
-        <h1 className="text-[2.8rem] pb-10 sm:pb-14 font-semibold font-[Roboto] text-center">
+      <div className="w-full place-items-center">
+        <h1 className="text-[2rem] sm:text-[2.8rem] pb-10 sm:pb-14 font-semibold font-[Roboto] text-center">
           Who We Are?
         </h1>
         <div className="w-full xl:w-[85%] 2xl:w-[75%] flex flex-col lg:flex-row justify-center gap-6 max-md:pl-4 max-md:pr-4">
           <div className=" w-full lg:w-1/2 flex  flex-col items-center justify-center gap-4">
-            <p className="text-lg sm:text-xl font-[Roboto] leading-6 max-sm:text-center">
+            <p className="text-lg sm:text-xl font-[Roboto] leading-6 max-sm:text-center text-white">
               TAN aims to set a new standard in the blockchain industry by
               offering solutions that address longstanding challenges L1s have
               been suffering from. offering solutions that address longstanding
             </p>
-            <p className="text-lg sm:text-xl font-[Roboto] leading-6 max-sm:text-center">
+            <p className="text-lg sm:text-xl font-[Roboto] leading-6 max-sm:text-center  text-white">
               TAN aims to set a new standard in the blockchain industry by
               offering solutions that address longstanding challenges L1s have
               been suffering from. offering solutions that address longstanding
             </p>
-            <p className="text-lg sm:text-xl font-[Roboto] leading-6 max-sm:text-center">
+            <p className="text-lg sm:text-xl font-[Roboto] leading-6 max-sm:text-center  text-white">
               TAN aims to set a new standard in the blockchain industry by
               offering solutions that address longstanding challenges L1s have
               been suffering from. offering solutions that address longstanding
             </p>
-            <p className="text-lg sm:text-xl font-[Roboto] leading-6 max-sm:text-center">
+            <p className="text-lg sm:text-xl font-[Roboto] leading-6 max-sm:text-center  text-white">
               TAN aims to set a new standard in the blockchain industry by
               offering solutions that address longstanding challenges L1s have
               been suffering from. offering solutions that address longstanding
@@ -37,8 +36,8 @@ const ListImageAndContainent = () => {
           </div>
 
           <div className="w-full lg:w-1/2 grid max-sm:place-items-center max-sm:gap-y-4 grid-cols-1 grid-rows-5 sm:grid-cols-3 sm:grid-rows-2">
-            <div className="h-[100%] bg-red-500 sm:row-start-1 sm:row-end-4">
-              <img className="h-[100%] object-cover" src={hrImage} />
+            <div className="bg-red-500 sm:row-start-1 sm:row-end-4">
+              <img className="h-[100%] object-cover" src={BlockImage1} />
             </div>
 
             <div className="bg-red-500">
@@ -54,7 +53,7 @@ const ListImageAndContainent = () => {
             </div>
 
             <div className=" bg-red-500">
-              <img className="h-[100%] object-cover" src={sun1Iamge} />
+              <img className="h-[100%] object-cover" src={BlockImage2} />
             </div>
           </div>
         </div>
